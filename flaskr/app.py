45839d4b844a60e6f86d9c07c96b9dd829bc87fb
Francisco <quicco.sousa@gmail.com>
2024-06-10 12:31:50 +0100

    Finish edit student functionality

--- flaskr/app.py
+++ flaskr/app.py
@@ -47,7 +47,7 @@ def login():
     return render_template("index.html")
 
 
-# Class Related Routes
+# ---Class Related Routes---
 @app.route("/select", methods=["GET", "POST"])
 def select():
     if request.method == "POST":
@@ -77,8 +77,8 @@ def select():
     return
 
 
-# Student Related Routes
-@app.route("/editstudent", methods=["GET", "POST"])
+# ---Student Related Routes---
+@app.route("/edit_student", methods=["GET", "POST"])
 def editstudent():
     if request.method == "POST":
         student_id = request.form.get("student_id")
@@ -112,3 +112,37 @@ def editstudent():
         finally:
             con.close()
     return render_template("")
+
+
+@app.route("/confirm_edit", methods=["GET", "POST"])
+def confirm_edit():
+    if request.method == "POST":
+        student_id = request.form.get("student_id")
+        upd_name = request.form.get("name")
+        upd_email = request.form.get("email")
+        upd_phone = request.form.get("phone")
+        upd_location = request.form.get("location")
+        upd_class_type = request.form.get("class_type")
+
+        # Return dicts instead of tuples
+        con = sqlite3.connect(db_path)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+
+        cur.execute(
+            """UPDATE student SET name = (?), email = (?), phone = (?), location = (?), class_type = (?) WHERE student_id = (?)""",
+            (
+                upd_name,
+                upd_email,
+                upd_phone,
+                upd_location,
+                upd_class_type,
+                student_id,
+            ),
+        )
+        con.commit()
+        cur.execute("SELECT * from student")
+        results = cur.fetchall()
+        students = [dict(row) for row in results]
+
+        return render_template("select.html", students=students)
